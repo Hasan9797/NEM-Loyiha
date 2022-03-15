9def65fbcd5dc39194efc8c26e03fa72f1da56a3
Hasanjon <hasanjonsadullayev97@gmail.com>
2022-03-15 17:19:16 +0500

    O'xshamadi edge file qo'shgandim

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://hasan:RSRSmqfUSsIKRb0A@cluster0.phuqw.mongodb.net/MEN-Loyiha")
+mongoose.connect("mongodb+srv://hasan:lGv4pJh14SLM9Qqh@cluster0.yroge.mongodb.net/MEN-Loyiha");
 
 app.use(express.static("public"));
 
 app.use(expresEdege.engine);
 app.set("views", `${__dirname}/views`);
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -26,6 +28,10 @@ app.get("/contact", (req, res) => {
 
 app.get("/post", (req, res) => {
     res.render("post");
+})
+
+app.get("posts/new", (req, res) => {
+    res.render("created");
 })
 
 app.listen(5000, () => {console.log("Server has been on PORT 5000...")});
